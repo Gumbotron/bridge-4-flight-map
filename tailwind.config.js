c9359070ba2bd1d9328165a4cde5002696b60774
Devon <75743950+Gumbotron@users.noreply.github.com>
2025-10-24 14:29:29 -0400

    Create tailwind.config.js with Stormlight-inspired color palette

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/**
+ * Tailwind CSS Configuration
+ */
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: '#00BFFF',
+        darkBlue: '#1E90FF',
+        lightGray: '#D3D3D3',
+        darkGray: '#696969',
+        gold: '#FFD700',
+        crimson: '#DC143C',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
